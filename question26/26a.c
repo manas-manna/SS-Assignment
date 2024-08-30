86dfdcc98689e5f4f4ac917b7d95d340083f22bb
manas-manna <madanmanna123456@gmail.com>
2024-08-30 23:36:34 +0530

    updated question26

--- question26/26a.c
+++ question26/26a.c
@@ -6,8 +6,8 @@ int main(int argc, char *argv[]) {
        fprintf(stderr, "Usage: %s <number1> <number2>\n", argv[0]);
         return 1;
     }
-
-    int a = atoi(argv[1]);
+    //agrv is of string form.We need to convert them to integer.
+    int a = atoi(argv[1]);   //atoi stands for ascii to  integer.It converts string to integer
     int b = atoi(argv[2]);
     int sum = a + b;
 
