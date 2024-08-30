a6fb8e837430213a62148ae1e04aae5d34150136
manas-manna <madanmanna123456@gmail.com>
2024-08-30 17:58:36 +0530

    added question21

---
+++ question21/21.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+int main() {
+    pid_t pid = fork();
+
+    if (pid < 0) {
+        // Error occurred during fork
+        fprintf(stderr, "Fork failed\n");
+        return 1;
+    } else if (pid == 0) {
+        // Child process
+        printf("Child process: PID = %d\n", getpid());
+    } else {
+        // Parent process
+        printf("Parent process: PID = %d\n", getpid() );
+    }
+
+    return 0;
+}
+
+
